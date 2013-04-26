76e0c177be33b0ab5bd18d854d4f40954a775e03
Jeff Byrnes <jeff@jeffbyrn.es>
2013-04-26 16:28:17 -0400

    Switch new Google Universal Analytics in prep for thejeffbyrnes.com
    Fancy new stuff from Google? We'll see.

--- public/index.php
+++ public/index.php
@@ -171,13 +171,16 @@
      */
     ?>
 
-    <!-- Asynchronous Google Analytics snippet.
-         mathiasbynens.be/notes/async-analytics-snippet -->
+    <!-- Google Universal Analytics beta
+         analytics.blogspot.com/2013/03/expanding-universal-analytics-into.html -->
     <script>
-        var _gaq=[['_setAccount','UA-583944-9'],['_setDomainName', 'jeffbyrn.es'],['_trackPageview']];
-        (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
-        g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
-        s.parentNode.insertBefore(g,s)}(document,'script'));
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+        ga('create', 'UA-583944-15', 'thejeffbyrnes.com');
+        ga('send', 'pageview');
     </script>
 
     <!-- Mint statistics -->
