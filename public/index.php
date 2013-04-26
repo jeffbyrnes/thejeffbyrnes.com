23644f98e14e9deeac0a1f5ee20bbe06787baff9
Jeff Byrnes <jeff@jeffbyrn.es>
2013-04-26 16:29:08 -0400

    Switch to new email address
    Moving away from jeff@jeffbyrn.es

--- public/index.php
+++ public/index.php
@@ -159,7 +159,7 @@
 
             <hr>
 
-            <a class="button" href="mailto:jeff@jeffbyrn.es?subject=Dropping a line from jeffbyrn.es">Drop me a line</a>
+            <a class="button" href="mailto:thejeffbyrnes@gmail.com?subject=Dropping a line from jeffbyrn.es">Drop me a line</a>
         </footer>
     </div><!-- end .container -->
 
