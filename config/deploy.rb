d2586815f18e0ce2718faf2c57c0a60c5bc8c5cf
Jeff Byrnes <jeff@jeffbyrn.es>
2014-03-03 17:12:31 -0500

    Remove unnecessary default value for keeping releases

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,6 @@ set :datadog_api_key, '3a10dbd29a7e52f9f75c76b7951564a7'
 
 set :linked_dirs, %w{public/.flickr-cache node_modules}
 
-set :keep_releases, 5
 
 # Ensure Grunt runs the `grunt release` command
 set :grunt_tasks, 'release'
