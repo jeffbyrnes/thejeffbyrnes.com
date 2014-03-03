7555923fa526227116af8585e499a2f0d3a7e58a
Jeff Byrnes <jeff@jeffbyrn.es>
2014-03-03 15:44:58 -0500

    Switch to Capistrano 3

--- Capfile
+++ Capfile
@@ -1,3 +1,13 @@
-require 'rubygems'
-require 'railsless-deploy'
-load    'config/deploy'
+# Load DSL and Setup Up Stages
+require 'capistrano/setup'
+
+# Includes default deployment tasks
+require 'capistrano/deploy'
+
+# Include Grunt tasks
+# require 'capistrano/grunt'
+
+set :stage, :production
+
+# Loads custom tasks from `lib/capistrano/tasks' if you have any defined.
+Dir.glob('lib/capistrano/tasks/*.cap').each { |r| import r }
--- config/deploy.rb
+++ config/deploy.rb
@@ -1,120 +1,41 @@
-# encoding: UTF-8
-
-# replace these with your server's information
-set :domain,  'thejeffbyrnes.com'
-set :user,    'ec2-user'
-
-# name this the same thing as the directory on your server
 set :application, 'thejeffbyrnes.com'
+set :repo_url, 'git@bitbucket.org:jeffbyrnes/thejeffbyrnes.com.git'
 
-# or use a hosted repository
-set :repository, 'git@bitbucket.org:jeffbyrnes/thejeffbyrnes.com.git'
+# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
 
-server "#{domain}", :app, :web, :db, primary: true
+server 'thejeffbyrnes.com', user: 'ec2-user', roles: %w{web app db}
 
-set :deploy_via, :copy
-set :copy_exclude, [
-  '.DS_Store',
-  '.git',
-  '.gitignore',
-  '.editorconfig',
-  '.gitattributes',
-  '.sass_cache',
-  'Capfile',
-  'config',
-  'config.rb',
-  'icon.png',
-  'Gruntfile.js',
-  'README.md',
-  'node_modules'
-]
+# you can set custom ssh options
+# it's possible to pass any option but you need to keep in mind that net/ssh understand limited list of options
+# you can see them in [net/ssh documentation](http://net-ssh.github.io/net-ssh/classes/Net/SSH.html#method-c-start)
+# set it globally
+set :ssh_options, { forward_agent: true }
 
-set :scm, :git
-set :branch, 'master'
-
-# Set where to deploy files
 set :deploy_to, "/srv/#{application}"
+set :scm, :git
 
-# Point symlink to public folder
-set :release_path, "#{releases_path}/#{release_name}"
+set :datadog_api_key, "3a10dbd29a7e52f9f75c76b7951564a7"
 
-# Don't use sudo for operations
-set :use_sudo, false
+# set :format, :pretty
+# set :log_level, :debug
+set :pty, true
 
-# Number of releases to keep
+# set :linked_files, %w{config/database.yml}
+# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
 set :keep_releases, 5
 
-# Tells Capistrano to create a local version of the repo and use that
-# to run deploys. Ensures it doesn't do a full clone each time
-set :deploy_via, :remote_cache
+# Ensure Grunt runs the `grunt release` command
+set :grunt_tasks, 'release'
 
-# Must be set for the password prompt from git to work
-default_run_options[:pty] = true
-
-ssh_options[:forward_agent] = true
-ssh_options[:paranoid]      = false
-
-# this tells capistrano what to do when you deploy
 namespace :deploy do
-  desc 'A macro-task that updates the code and fixes the symlink.'
-  task :default do
-    transaction do
-      update_code
-      create_symlink
-    end
-  end
 
-  task :update_code, except: { no_release: true } do
-    on_rollback { run "rm -rf #{release_path}; true" }
-    strategy.deploy!
-  end
+  after :finishing, 'deploy:cleanup'
 
-  task :after_deploy do
-    cleanup
-  end
 end
 
-namespace :build do
-  desc 'Handle all compilation, minification, and deployment of assets'
-  task :default do
-    # optional way to skip build & just upload current assets to the servers
-    skip_build = fetch(:skip_build, false)
-
-    build unless skip_build
-    upload_build
-  end
-
-  desc 'Wrapper command for `grunt release`'
-  task :build, roles: :web, except: { no_release: true } do
-    # Build site using Grunt.js
-    run_locally('grunt release')
-  end
-
-  desc 'Uploads compiled release'
-  task :upload_build, roles: :web, except: { no_release: true } do
-    asset_dirs = ['public/css', 'public/js']
-    skip_build = fetch(:skip_build, false)
-
-    logger.debug 'Uploading compiled release'
-    asset_dirs.each do |dir|
-      logger.debug "trying to upload assets from ./#{dir}/ -> #{latest_release}/#{dir}/"
-
-      upload("#{dir}", "#{latest_release}/public/", via: :scp, recursive: true)
-    end
-  end
-end
-
-namespace :shared do
-  task :make_shared_dir do
-    run "if [ ! -d #{shared_path}/files ]; then mkdir #{shared_path}/files; fi"
-  end
-
-  task :make_symlinks do
-    run "if [ ! -h #{release_path}/shared ]; then ln -s #{shared_path}/files/ #{release_path}/shared; fi"
-    run "for p in `find -L #{release_path}/public -type l`; do t=`readlink $p | grep -o 'shared/.*$'`; mkdir -p #{release_path}/public/$t; sudo chown apache:ec2-user #{release_path}/public/$t; done"
-  end
-end
-
-after 'deploy:update_code', 'build'
-after 'deploy:update_code', 'shared:make_shared_dir'
-after 'deploy:update_code', 'shared:make_symlinks'
+# before :updated, 'grunt'
+# before :updated, 'build'
+# before :updated, 'shared:make_shared_dir'
+# before :updated, 'shared:make_symlinks'
