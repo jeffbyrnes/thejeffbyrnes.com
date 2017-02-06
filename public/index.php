151973c1c55376255dcdd7dee8d7cb67609419e9
Jeff Byrnes <thejeffbyrnes@gmail.com>
2017-02-06 16:17:12 -0500

    Update Google Analytics snippet
    Match the official bit identically

--- public/index.php
+++ public/index.php
@@ -147,15 +147,13 @@
         <p>© 2012–<?php echo date('Y'); ?> Jeff Byrnes</p>
     </div>
 
-    <!-- Google Universal Analytics beta
-         analytics.blogspot.com/2013/03/expanding-universal-analytics-into.html -->
     <script>
         (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
         (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
         m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-        ga('create', 'UA-583944-15', 'thejeffbyrnes.com');
+        ga('create', 'UA-583944-15', 'auto');
         ga('send', 'pageview');
     </script>
 
