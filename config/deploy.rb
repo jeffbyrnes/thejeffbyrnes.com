76f77383476201ff2f34f871076c6123867df05b
Jeff Byrnes <thejeffbyrnes@gmail.com>
2017-07-10 13:11:01 -0400

    Drop reload of PHP-FPM
    Breaks deployments. Sort it out later.

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,4 +8,3 @@ set :datadog_api_key, '3a10dbd29a7e52f9f75c76b7951564a7'
 set :linked_dirs, %w(public/.flickr-cache)
 
 before 'deploy:finishing', 'grunt:default'
-after 'deploy:finishing', 'php_fpm:reload'
